43ad4020646910c96134b14b923be9b9851d8df7
“Francine” <francinehouston@gmail.com>
2025-10-31 06:39:34 -0400

    adding resume to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,45 @@ import { About } from './components/sections/About';
 import { Projects } from './components/sections/Projects';
 import { Contact } from './components/sections/Contact';
 import { ResumeButton } from './components/sections/Resume';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 
 function App() {
  const [isLoaded, setIsLoaded] = useState(false);
  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-<>
+    <Router basename="/portfolio-app"> {/* Important for GitHub Pages */}
+
+
+
 { !isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{""}
   <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`} >
-    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+   
+   <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+    <Routes>
+    {/* Main page */}
+
+    <Route 
+    path="/" 
+    element={
+      <>
     <Home/>
     <About/>
     <Projects/>
     <Contact/>
-    {/*Add this section wrapper*/}
-    <section id="resume">
-    <ResumeButton/>
-    </section>
-  </div>
-</>
+    </>
+    }
+    />
+
+    {/*Resume page*/}
+    <Route path="/resume" element={<ResumeButton />} />
+   
+ 
+    </Routes>
+    </div>
+    </Router>
   );
     
   
